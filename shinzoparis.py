093c18f1814581a6515f56982b5dc6cc6ee6fc60
fayeezus <cs.lemon@live.com>
2018-01-15 19:13:13 -0500

    Update shinzoparis.py

--- shinzoparis.py
+++ shinzoparis.py
@@ -9,6 +9,10 @@ form_url = 'https://raffle.shinzo.paris/levis-x-air-jordan-4-denim/'
 headers = {'User-Agent':
            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
+print("ShinzoParis Raffle Script by Fayeezus")
+print("Starting now...")
+
+
 session = requests.Session()
 session.headers.update(headers)
 
